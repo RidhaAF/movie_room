33b1929ece83435fb81f729500d9b33dc6717e9b
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-12-13 01:11:31 +0700

    Add Sign In Page

---
+++ lib/pages/auth/sign_in_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movie_room/themes/theme.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget title() {
+      return Text(
+        'Welcome to\nMovie Room🍿',
+        style: TextStyle(
+          fontSize: 36,
+          fontWeight: bold,
+          color: whiteColor,
+        ),
+      );
+    }
+
+    Widget subtitle() {
+      return Text(
+        'We give you the best recommendation movie and series.',
+        style: TextStyle(
+          fontSize: 12,
+          color: secondaryColor,
+        ),
+      );
+    }
+
+    Widget illustration() {
+      return Align(
+        alignment: Alignment.center,
+        child: Image.asset(
+          'assets/images/il_sign_in.png',
+          width: 248,
+        ),
+      );
+    }
+
+    Widget signInButton() {
+      return ElevatedButton(
+        onPressed: () {},
+        style: darkGrayButtonStyle,
+        child: Container(
+          height: 48,
+          width: double.infinity,
+          alignment: Alignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/ic_google.png',
+                width: 36,
+              ),
+              const SizedBox(width: 8),
+              Text(
+                'Sign in with Google',
+                style: GoogleFonts.inter(
+                  color: whiteColor,
+                  fontSize: 18,
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(defaultMargin),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              title(),
+              subtitle(),
+              const SizedBox(height: 170),
+              illustration(),
+              const SizedBox(height: 170),
+              signInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
